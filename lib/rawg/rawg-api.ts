7e8fe58e49ac7f5df3ed6f6733e2b033c3152bd1
Paul Bamberg <jpbamberg1993@gmail.com>
2023-10-02 17:31:25 -0500

    Improve the way query strings are built up in js

--- lib/rawg/rawg-api.ts
+++ lib/rawg/rawg-api.ts
@@ -30,10 +30,11 @@ export type RawgPlatform = {
 }
 
 export class RawgApi {
+	// Todo: Figure out a cleaner way to build up queries
 	private readonly BASE_URL: string = 'https://api.rawg.io/api'
 
 	public async getPlatforms(): Promise<RawgPlatform[]> {
-		const response = await this.sendRequest(`platforms`)
+		const response = await this.sendRequest(`/platforms`)
 		const json = await response.json()
 		if (json.next) {
 			const nextPageResponse = await fetch(json.next)
@@ -44,20 +45,34 @@ export class RawgApi {
 	}
 
 	public async getGenres(): Promise<RawgGenre[]> {
-		const response = await this.sendRequest(`genres`)
+		const response = await this.sendRequest(`/genres`)
 		const json = await response.json()
 		return json.results
 	}
 
-	public async getGames(): Promise<RawgGame[]> {
-		const response = await this.sendRequest(`games`)
-		const json = await response.json()
-		return json.results
+	public async getGames(page: number): Promise<RawgGame[]> {
+		try {
+			const response = await this.sendRequest(
+				`/games`,
+				new URLSearchParams({ page: page.toString() })
+			)
+			const json = await response.json()
+			return json.results
+		} catch (e) {
+			console.error(e)
+			return []
+		}
 	}
 
-	private async sendRequest(route: string) {
-		return await fetch(
-			`${this.BASE_URL}/${route}?key=${process.env.RAWG_API_KEY}`
-		)
+	private async sendRequest(route: string, queryParameters?: URLSearchParams) {
+		const rawgUrl = new URL(this.BASE_URL + route)
+		const searchParams = rawgUrl.searchParams
+		searchParams.append('key', process.env.RAWG_API_KEY ?? '')
+		if (queryParameters) {
+			for (const [key, value] of queryParameters.entries()) {
+				searchParams.append(key, value)
+			}
+		}
+		return await fetch(rawgUrl.toString())
 	}
 }
