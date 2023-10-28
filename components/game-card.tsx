70262705d4e72ffc4b2e4bd2624500c91b3bb5af
Paul Bamberg <jpbamberg1993@gmail.com>
2023-10-28 16:33:54 -0500

    Update game card to use the next Image component

--- components/game-card.tsx
+++ components/game-card.tsx
@@ -1,6 +1,7 @@
 import { Game } from '@/lib/db/schema/games'
 import { PlatformIconList } from '@/components/platform-icon-list'
 import { getCroppedImageUrl } from '@/lib/utils'
+import Image from 'next/image'
 
 type Props = {
 	game: Game
@@ -9,11 +10,14 @@ type Props = {
 export function GameCard({ game }: Props) {
 	return (
 		<div className='transform overflow-hidden rounded-lg border border-gray-200 bg-white shadow transition-transform hover:scale-105 dark:border-gray-700 dark:bg-cyan-950'>
-			<img
-				src={getCroppedImageUrl(game.backgroundImage)}
-				alt={game.name}
-				className='aspect-w-16 aspect-h-9'
-			/>
+			<div className='relative h-60 w-full'>
+				<Image
+					src={getCroppedImageUrl(game.backgroundImage)}
+					alt={game.name}
+					layout='fill'
+					objectFit='cover'
+				/>
+			</div>
 			<div className='p-6'>
 				<PlatformIconList platforms={game.platforms} />
 			</div>
