c518ebfd72da97d9a51705a6a8c0f43c0b579679
Paul Bamberg <jpbamberg1993@gmail.com>
2023-12-01 00:03:29 -0500

    Style active genre

--- components/genres-list.tsx
+++ components/genres-list.tsx
@@ -17,23 +17,33 @@ export function GenresList({ genres, selectedGenre }: Props) {
 		<div className='pt-2'>
 			<h2 className='pb-2 text-xl font-bold dark:text-white'>Genres</h2>
 			<ul>
-				{genres.map((g) => (
-					<li key={g.id} className='flex items-center pb-2'>
-						<Image
-							src={g.imageBackground}
-							alt={g.name}
-							width={32}
-							height={32}
-							className='h-[32px] w-[32px] rounded-lg object-cover'
-						/>
-						<button
-							onClick={() => setGenre(g.slug)}
-							className='pl-2 text-gray-400'
+				{genres.map((g) => {
+					const activeGenre = genre === g.slug
+					return (
+						<li
+							key={g.id}
+							className={`flex items-center py-1.5 pl-1 ${
+								activeGenre && 'border border-neon'
+							}`}
 						>
-							{g.name}
-						</button>
-					</li>
-				))}
+							<Image
+								src={g.imageBackground}
+								alt={g.name}
+								width={32}
+								height={32}
+								className='h-[32px] w-[32px] rounded-lg object-cover'
+							/>
+							<button
+								onClick={() => setGenre(g.slug)}
+								className={`pl-2 text-gray-400 ${
+									activeGenre && 'font-bold text-gray-700 dark:text-white'
+								}`}
+							>
+								{g.name}
+							</button>
+						</li>
+					)
+				})}
 			</ul>
 		</div>
 	)
